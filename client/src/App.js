702f83709f9add4e6773545d7a5cc91a41098cd4
amir <amirbayat1998@yahoo.com>
2019-06-22 23:53:17 +0430

    finished register and waiting for redux

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,8 @@ import React, {Component} from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import './App.css';
 import UserPage from './component/UserPage';
-import Register from "./component/register";
+import Register from "./component/Register";
+import Main from './component/Main';
 // import {Route} from "react-router";
 // import {BrowserRouter} from "react-router-dom";
 // import {BrowserRouter as Router, Route} from 'react-router-dom';
@@ -21,7 +22,7 @@ class App extends Component {
         return (
             <Router>
                 <div>
-                    <Route exact path='/' component={UserPage} />
+                    <Route exact path='/' component={Main} />
                     <section className="container">
                         <Switch>
                             <Route exact path={'/register'} component={Register} />
