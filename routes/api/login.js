6276652174bf373765094ae453dd10d0d720dca0
amir <amirbayat1998@yahoo.com>
2019-07-03 11:50:26 +0430

    starting te create live session

--- routes/api/login.js
+++ routes/api/login.js
@@ -6,6 +6,23 @@ const bcrypt = require('bcryptjs');
 const User = require('../../models/user');
 const config = require('config');
 const jwt = require('jsonwebtoken');
+const auth = require('../../middleware/auth');
+
+
+
+// @route    GET api/auth
+// @desc     Test route
+// @access   Public
+router.get('/', auth, async (req, res) => {
+    try {
+        const user = await User.findById(req.user.id).select('-password');
+        res.json(user);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
 
 
 router.post('/', [
