0a78f1c12465f422ebd5bd7a793a57f38e23a9f0
amir <amirbayat1998@yahoo.com>
2019-07-11 01:53:40 +0430

    commited for creating new branch

--- client/src/component/StoreCourse.js
+++ client/src/component/StoreCourse.js
@@ -3,29 +3,34 @@ import propTypes from 'prop-types';
 import {connect} from "react-redux";
 import {storecourse} from '../actions/storecourse';
 
-const StoreCourse = ({courses, storeLoaded, storecourse, user}) => {
+const StoreCourse = ({miniCourses, storeLoaded, storecourse, user, loading, miniCourseLoading}) => {
     useEffect(() => {
-        storecourse();
-    },[]);
+        if(user !== '')
+            storecourse();
+    },[user]);
 
-    return(
+    return(  !loading && user !== '' && !miniCourseLoading ? (
         <Fragment>
-            {courses.map(item => <img src={item.picture}/>)}
-        </Fragment>
+            {miniCourses.map(item => <img src={item.picture}/>)}
+        </Fragment> ) : (<Fragment><p>not yet baby!</p></Fragment>)
     );
 };
 
 StoreCourse.propTypes = {
-    courses: propTypes.array.isRequired,
+    miniCourses: propTypes.array.isRequired,
     storeLoaded: propTypes.bool.isRequired,
     storecourse: propTypes.func.isRequired,
-    user: propTypes.object.isRequired
+    user: propTypes.object.isRequired,
+    loading: propTypes.bool.isRequired,
+    miniCourseLoading: propTypes.bool.isRequired
 };
 
 const mapStateToProps = state => ({
-    courses: state.storecourse.courses,
     storeLoaded: state.storecourse.storeLoaded,
-    user: state.auth.user
+    user: state.auth.user,
+    loading: state.auth.loading,
+    miniCourses: state.getMiniCourse.miniCourses,
+    miniCourseLoading: state.getMiniCourse.loading
 });
 
 export default connect(mapStateToProps, {storecourse})(StoreCourse);
--- routes/api/storecourse.js
+++ routes/api/storecourse.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const Course = require('../../models/course');
+const MiniCourse = require('../../models/miniCourse');
 
 router.post('/', async (req, res) => {
     try {
 
         const tag = req.body.tag;
         const regexp = new RegExp(tag);
-        const courses = await Course.find({"tags": regexp});
-        res.send(courses);
+        const MiniCourses = await Course.find({"tags": regexp});
+        res.send(MiniCourses);
     } catch (err) {
         console.log(err);
         res.status(500).send("server error");
